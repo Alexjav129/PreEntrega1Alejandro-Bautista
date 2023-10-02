154e40a09741a05ccf6bf65b0a2696608ca973a8
abautista <alexjav.12@hotmail.es>
2023-10-02 11:34:14 -0600

    add 2 options of interactive simulators

---
+++ Simulador Ineractivo Op2/main.js
@@ -0,0 +1,38 @@
+while (true) {
+  let currency = prompt(
+    "Indica a la moneda que quieres convertir a USD, ejemplo: (MXN, ARS, COL, VES, GTQ) o escribe 'salir' para terminar"
+  );
+
+  if (currency.toLowerCase() === "salir") {
+    break;
+  }
+
+  let value = parseFloat(
+    prompt("Indica el monto de dólares que quieres convertir")
+  );
+
+  let result = 0;
+
+  switch (currency.toUpperCase()) {
+    case "MXN":
+      result = (value * 17.62).toFixed(2);
+      break;
+    case "ARS":
+      result = (value * 350.04).toFixed(2);
+      break;
+    case "COL":
+    case "COP":
+      result = (value * 4153.65).toFixed(2);
+      break;
+    case "VES":
+      result = (value * 34.2214).toFixed(2);
+      break;
+    case "GTQ":
+      result = (value * 7.86).toFixed(2);
+      break;
+    default:
+      result = "Moneda no Válida";
+      break;
+  }
+  alert(`Tu conversion de dólares a tu moneda es: ${result}`);
+}
---
+++ Simulador Interactivo Op1/main.js
@@ -0,0 +1,38 @@
+let numRomano = prompt(
+  "Ingresa un Número Romano válido para convertirlo en un Número Árabigo, ej: MCMXCIX regresa 1999"
+);
+let sum = 0;
+for (let i = 0; i <= numRomano.length; i++) {
+  let before = numRomano[i - 1];
+  switch (numRomano[i]) {
+    case "I":
+      sum += 1;
+      break;
+    case "V":
+      sum = before === "I" ? sum + 3 : sum + 5;
+      break;
+    case "X":
+      sum = before === "I" ? sum + 8 : sum + 10;
+      break;
+    case "L":
+      sum = before === "X" ? sum + 30 : sum + 50;
+      break;
+    case "C":
+      sum = before === "X" ? sum + 80 : sum + 100;
+      break;
+    case "D":
+      sum = before === "C" ? sum + 300 : sum + 500;
+      break;
+    case "M":
+      sum = before === "C" ? sum + 800 : sum + 1000;
+      break;
+  }
+}
+alert(`El valor de tu número romano es: ${sum}`);
+
+/* 
+Las unicas restricciones para crear el algoritmo son:
+I se puede colocar antes de V (5) y X (10) para dar 4 y 9. 
+X se puede colocar antes de L (50) y C (100) para dar 40 y 90. 
+C se puede colocar antes de D (500) y M (1000) para dar 400 y 900.
+*/
