7f32296af4d918c7d0a70ce144b41bb1a06c30d9
abautista <alexjav.12@hotmail.es>
2023-10-02 18:08:04 -0600

    wrap both js code files in a js function

--- Simulador Ineractivo Op2/main.js
+++ Simulador Ineractivo Op2/main.js
@@ -1,38 +1,42 @@
-while (true) {
-  let currency = prompt(
-    "Indica a la moneda que quieres convertir a USD, ejemplo: (MXN, ARS, COL, VES, GTQ) o escribe 'salir' para terminar"
-  );
+function currencyConverter() {
+  while (true) {
+    let currency = prompt(
+      "Indica a la moneda que quieres convertir a USD, ejemplo: (MXN, ARS, COL, VES, GTQ) o escribe 'salir' para terminar"
+    );
 
-  if (currency.toLowerCase() === "salir") {
-    break;
+    if (currency.toLowerCase() === "salir") {
+      break;
+    }
+
+    let value = parseFloat(
+      prompt("Indica el monto de dólares que quieres convertir")
+    );
+
+    let result = 0;
+
+    switch (currency.toUpperCase()) {
+      case "MXN":
+        result = (value * 17.62).toFixed(2);
+        break;
+      case "ARS":
+        result = (value * 350.04).toFixed(2);
+        break;
+      case "COL":
+      case "COP":
+        result = (value * 4153.65).toFixed(2);
+        break;
+      case "VES":
+        result = (value * 34.2214).toFixed(2);
+        break;
+      case "GTQ":
+        result = (value * 7.86).toFixed(2);
+        break;
+      default:
+        result = "Moneda no Válida";
+        break;
+    }
+    alert(`Tu conversion de dólares a tu moneda es: ${result}`);
   }
-
-  let value = parseFloat(
-    prompt("Indica el monto de dólares que quieres convertir")
-  );
-
-  let result = 0;
-
-  switch (currency.toUpperCase()) {
-    case "MXN":
-      result = (value * 17.62).toFixed(2);
-      break;
-    case "ARS":
-      result = (value * 350.04).toFixed(2);
-      break;
-    case "COL":
-    case "COP":
-      result = (value * 4153.65).toFixed(2);
-      break;
-    case "VES":
-      result = (value * 34.2214).toFixed(2);
-      break;
-    case "GTQ":
-      result = (value * 7.86).toFixed(2);
-      break;
-    default:
-      result = "Moneda no Válida";
-      break;
-  }
-  alert(`Tu conversion de dólares a tu moneda es: ${result}`);
 }
+
+currencyConverter();
--- Simulador Interactivo Op1/main.js
+++ Simulador Interactivo Op1/main.js
@@ -1,34 +1,45 @@
-let numRomano = prompt(
-  "Ingresa un Número Romano válido para convertirlo en un Número Árabigo, ej: MCMXCIX regresa 1999"
-);
-let sum = 0;
-for (let i = 0; i <= numRomano.length; i++) {
-  let before = numRomano[i - 1];
-  switch (numRomano[i]) {
-    case "I":
-      sum += 1;
+function romanToNormalNumber() {
+  while (true) {
+    let numRomano = prompt(
+      "Ingresa un Número Romano válido para convertirlo en un Número Árabigo, ej: MCMXCIX regresa 1999, o escribe 'salir' para terminar"
+    ).toUpperCase();
+
+    if (numRomano.toLowerCase() === "salir") {
       break;
-    case "V":
-      sum = before === "I" ? sum + 3 : sum + 5;
-      break;
-    case "X":
-      sum = before === "I" ? sum + 8 : sum + 10;
-      break;
-    case "L":
-      sum = before === "X" ? sum + 30 : sum + 50;
-      break;
-    case "C":
-      sum = before === "X" ? sum + 80 : sum + 100;
-      break;
-    case "D":
-      sum = before === "C" ? sum + 300 : sum + 500;
-      break;
-    case "M":
-      sum = before === "C" ? sum + 800 : sum + 1000;
-      break;
+    }
+
+    let sum = 0;
+    for (let i = 0; i <= numRomano.length; i++) {
+      let before = numRomano[i - 1];
+      switch (numRomano[i]) {
+        case "I":
+          sum += 1;
+          break;
+        case "V":
+          sum = before === "I" ? sum + 3 : sum + 5;
+          break;
+        case "X":
+          sum = before === "I" ? sum + 8 : sum + 10;
+          break;
+        case "L":
+          sum = before === "X" ? sum + 30 : sum + 50;
+          break;
+        case "C":
+          sum = before === "X" ? sum + 80 : sum + 100;
+          break;
+        case "D":
+          sum = before === "C" ? sum + 300 : sum + 500;
+          break;
+        case "M":
+          sum = before === "C" ? sum + 800 : sum + 1000;
+          break;
+      }
+    }
+    alert(`El valor de tu número romano es: ${sum}`);
   }
 }
-alert(`El valor de tu número romano es: ${sum}`);
+
+romanToNormalNumber();
 
 /* 
 Las unicas restricciones para crear el algoritmo son:
